4892ba954a48807940d26440c853e4f8f20bf333
Hugo Gomes <hdsg0807@gmail.com>
2020-01-24 18:00:48 -0300

    First Commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const app = require('./src/config/custom-express');
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
+
+app.get('/', (req, resp) => {
+    resp.send(
+        '<html>'+
+            '<head>'+
+                '<meta charset="utf-8" />'+
+            '</head>'+
+            '<body>'+
+                '<h1>Casa do Código</h1>'+
+            '</body>'+
+        '</html>'
+    )
+});
+
+app.get('/livros', (req, resp) => {
+    resp.send(
+        '<html>'+
+            '<head>'+
+                '<meta charset="utf-8" />'+
+            '</head>'+
+            '<body>'+
+                '<h1>Listagem de Livros</h1>'+
+            '</body>'+
+        '</html>'
+    )
+});
+
+// const http = require('http');
+
+// const servidor = http.createServer(function (req, resp) {
+
+//     if (req.url == '/') {
+//         html = '<html>'+
+//         '<head>'+
+//             '<meta charset="utf-8" />'+
+//         '</head>'+
+//         '<body>'+
+//             '<h1>Casa do Código</h1>'+
+//         '</body>'+
+//     '</html>';
+//     }else if (req.url == '/livros') {
+//         html = '<html>'+
+//         '<head>'+
+//             '<meta charset="utf-8" />'+
+//         '</head>'+
+//         '<body>'+
+//             '<h1>Listagem de Livros</h1>'+
+//         '</body>'+
+//     '</html>';
+//     }
+//     resp.end(html);
+// });
+
+// servidor.listen(3000, 'localhost');
